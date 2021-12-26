17df22e5184575d2c1ea6983ff43ef8fbfdf962a
Ricardo Gabriel Ramírez Arriaga <a01338143@itesm.mx>
2021-12-25 19:06:16 -0600

    porocheck app set up

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import Image from 'next/image'
 
 export default function Home({ allPostsData }) {
   return (
@@ -12,11 +13,19 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>PoroCheck is a Discord bot that helps you inspect basic stats from a summoner in League of Legends.</p>
+        <p>The <code>!whois</code> command takes in two arguments, the region following the summoner name of the player you want to lookup.</p>
+        <code>!whois REGION SUMMONER</code>
+        <p>Example: <code>!whois na Doublelift</code></p>
+        <Image
+              priority
+              src="/images/poro-test.png"
+              height={326}
+              width={311}
+            />
+        <h2 className={utilStyles.headingLg}>Bot Invite</h2>
+        <p>To use the PoroCheck paste this invitation link on your browser:</p>
+        <p class={utilStyles.linkClass} style={{backgroundColor: 'lightGrey', padding: '10px'}}><code>https://discord.com/api/oauth2/authorize?client_id=922920077567864953&permissions=8&scope=bot</code></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -34,6 +43,10 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <h2 class={utilStyles.headingLg}>Source Code</h2>
+      <Link href='https://github.com/elrick97/riotBot'>
+        <a>Github repo</a>
+      </Link>
     </Layout>
   )
 }
---
+++ posts/TODOs.md
@@ -0,0 +1,12 @@
+---
+title: "TODOs"
+date: "2021-12-25"
+---
+
+This bot is a work in progress.
+
+Current plans:
+
+- [ ] Display Top 3 Champions with respective win rates from last 20 games.
+- [ ] Enable default region (NA) and be able to change it (per server).
+- [ ] Enable Option to look up a summoner in a given queue (solo/queue).
